29f04ad31cd5253f7560bb689f5945f56b0fbc0b
mwalcott1 <miles_walcott@yahoo.com>
2026-04-23 11:31:57 -0600

    Added second river extraction tool for time frame calibration purposes

--- centerlineExtraction1.py
+++ centerlineExtraction1.py
@@ -1,5 +1,6 @@
 import pynhd
 from shapely.geometry import box
+import os
 
 bounds = (-104.54089, 38.25403, -104.44024, 38.27055)
 
@@ -15,7 +16,9 @@ arkansas = boundedData[boundedData['gnis_name'] == 'Arkansas River']
 
 # this if/else was useful for debugging but also could be a useful sanity check if we ever decide to use another location
 if not arkansas.empty:
-    arkansas.to_file('arkansasCenterlineUSGS.geojson', driver='GeoJSON')
+    arkansas.to_file('arkansasCenterlineUSGS1980.geojson', driver='GeoJSON')
     print("Found and saved data successfully")
 else:
     print("No data found for given river in given bounds")
+
+os.remove('arkansas_high_res.tif')
